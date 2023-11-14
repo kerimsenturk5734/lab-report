ba3e7742b4288c814bb9f74c452bbff1524ca462
Kerim Şentürk <kerimsenturk2002@outlook.com>
2023-11-14 18:05:07 +0100

    Spring Boot Project Initialized

---
+++ src/main/java/com/kerimsenturk/labreport/LabReportManagerApplication.java
@@ -0,0 +1,13 @@
+package com.kerimsenturk.labreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabReportManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabReportManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kerimsenturk/labreport/LabReportManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerimsenturk.labreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabReportManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

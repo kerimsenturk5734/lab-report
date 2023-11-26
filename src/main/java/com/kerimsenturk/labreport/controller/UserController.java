c847d74b434c80b68713ba89128206d274eb4341
Kerim Şentürk <kerimsenturk2002@outlook.com>
2023-11-26 16:24:40 +0100

    register endpoint validated.

--- src/main/java/com/kerimsenturk/labreport/controller/UserController.java
+++ src/main/java/com/kerimsenturk/labreport/controller/UserController.java
@@ -8,6 +8,7 @@ import com.kerimsenturk.labreport.service.UserService;
 import com.kerimsenturk.labreport.util.MessageBuilder;
 import com.kerimsenturk.labreport.util.Result.SuccessDataResult;
 import com.kerimsenturk.labreport.util.Result.SuccessResult;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -23,18 +24,20 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
         this.messageBuilder = new MessageBuilder();
+
     }
 
     //This endpoint asks some authorizes to access
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody CreateUserRequest createUserRequest){
+    public ResponseEntity<?> register(@Valid @RequestBody CreateUserRequest createUserRequest){
         String id = userService.register(createUserRequest);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
+
         return ResponseEntity.created(uri).build();
     }
 
     @PostMapping("/registerPatient")
-    public ResponseEntity<?> registerPatient(@RequestBody PatientCreateRequest patientCreateRequest){
+    public ResponseEntity<?> registerPatient(@Valid @RequestBody PatientCreateRequest patientCreateRequest){
         String id = userService.registerPatient(patientCreateRequest);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
         return ResponseEntity.created(uri).build();
--- src/main/java/com/kerimsenturk/labreport/dto/request/CreateUserRequest.java
+++ src/main/java/com/kerimsenturk/labreport/dto/request/CreateUserRequest.java
@@ -1,12 +1,34 @@
 package com.kerimsenturk.labreport.dto.request;
 
 import com.kerimsenturk.labreport.model.enums.UserRole;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
 
 public record CreateUserRequest(
+        @NotBlank
+        @NotEmpty
+        @Length(min = 7, max = 11) //userId has 7 digit for hospital personals, 11 digit for patients.
+        @Pattern(regexp = "[0-9]+")
         String userId,
+
+        @NotBlank
+        @NotEmpty
         String name,
+
+        @NotBlank
+        @NotEmpty
         String surname,
+
+        @NotBlank
+        @NotEmpty
+        @Length(min = 7, max = 20)
+        //Pattern working but it can not accept any string. I think it is about UTF format
+        //@Pattern(regexp = "(?=.*?[a-z]+)(?=.*?[A-Z]+)(?=.*?[0-9]+)")
         String password,
+
+        //It may be necessary to create a new EnumValidator Annotation
         UserRole userRole) {
 
 }

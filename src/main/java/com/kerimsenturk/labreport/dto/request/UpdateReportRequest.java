a49007ecd92f693af9f4bb26b933017cd99fc834
Kerim Şentürk <kerimsenturk2002@outlook.com>
2023-12-17 21:26:15 +0100

    UpdateReportRequest validated.

--- src/main/java/com/kerimsenturk/labreport/dto/request/UpdateReportRequest.java
+++ src/main/java/com/kerimsenturk/labreport/dto/request/UpdateReportRequest.java
@@ -1,9 +1,16 @@
 package com.kerimsenturk.labreport.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
 import java.util.Optional;
 
 public record UpdateReportRequest(
         Optional<String> reportId,
-        Optional<String> title,
-        Optional<String> details) {
+        Optional<
+                @NotBlank
+                @Length(min = 5, max = 50) String> title,
+        Optional<
+                @NotBlank
+                @Length(min = 20, max = 200)String> details) {
 }

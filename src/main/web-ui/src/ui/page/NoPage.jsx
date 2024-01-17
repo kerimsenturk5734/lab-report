c6033047896af0e98defad80bd4f1e9c10093988
Kerim Şentürk <kerimsenturk2002@outlook.com>
2024-01-17 15:43:18 +0100

    not found page created

--- src/main/web-ui/src/ui/page/NoPage.jsx
+++ src/main/web-ui/src/ui/page/NoPage.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import {MDBCard, MDBContainer} from "mdb-react-ui-kit";
+import {Link} from "react-router-dom";
+import {PagePath} from "../../App";
 
-function NoPage(props) {
+function NoPage() {
     return (
-        <div>Page not found</div>
+        <MDBContainer className="mt-5" fluid={false}>
+            <MDBCard className={"p-5"}>
+                <div className={"d-flex row justify-content-center text-center gap-5"}>
+                    <img
+                        src={"https:/cdn-icons-png.flaticon.com/512/3855/3855833.png"}
+                        alt={"404"}
+                        className={"w-responsive w-25"}/>
+                    <span className={"text-xx-large"}>
+                        Page Not Found
+                    </span>
+                    <Link to={PagePath.LOGIN}>
+                        <div className={"btn text-x-large"}>
+                            <i className={"fa fa-solid fa-arrow-right"}></i>
+                            <span>   Go to Login</span>
+                        </div>
+                    </Link>
+                </div>
+            </MDBCard>
+        </MDBContainer>
     );
 }
 

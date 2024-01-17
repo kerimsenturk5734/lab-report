6c1c0dd4a5d35c822e44a7d217ccc04883672765
Kerim Şentürk <kerimsenturk2002@outlook.com>
2024-01-17 12:52:43 +0100

    MyReports page created

--- src/main/web-ui/src/ui/components/Content.jsx
+++ src/main/web-ui/src/ui/components/Content.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Table from "./Table";
 import {ContentType, UserType} from "./Constants";
 import Profile from "./Profile";
+import MyReports from "./MyReports";
 
 export default function Content({userType = UserType.DEFAULT, contentType = ContentType.DEFAULT}) {
     return (
@@ -18,13 +19,13 @@ export default function Content({userType = UserType.DEFAULT, contentType = Cont
 }
 
 
-
 function ContentFactory({contentType = ContentType, userType = UserType}) {
     switch (contentType) {
         case ContentType.TABLE :
             return <Table userType={userType}/>;
         case ContentType.PROFILE : return <Profile/>;
         case ContentType.CREATE_REPORT : return <div>Create Report</div>;
+        case ContentType.MY_REPORTS : return <MyReports/>
         default: return <>Wrong Selection -> ${contentType}</>
     }
 }
---
+++ src/main/web-ui/src/ui/components/MyReports.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {MDBCard} from "mdb-react-ui-kit";
+
+function MyReports(props) {
+    return (
+        <div>
+            <div className={"d-flex row justify-content-center"}>
+                <div className={"row text-black-50"}>
+                    <ReportCard/>
+                    <ReportCard/>
+                    <ReportCard/>
+                    <ReportCard/>
+                    <ReportCard/>
+                    <ReportCard/>
+                    <ReportCard/>
+                    <ReportCard/>
+                    <ReportCard/>
+                    <ReportCard/>
+                    <ReportCard/>
+                    <ReportCard/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function ReportCard(){
+    return (
+        <MDBCard className={"col-2 m-4 p-2 gap-2"}>
+            <div className={"d-flex justify-content-center"}>
+                <i className="fa fa-solid fa-file fa-8x"></i>
+            </div>
+            <div className={"d-flex justify-content-center"}>
+                <h6 className={"text-black-50"}>
+                    <span>2023 Sep 16 2:13 PM UTC+3 </span>
+                    <i className="fa fa-solid fa-calendar "></i>
+                </h6>
+            </div>
+            <div className={"d-flex justify-content-around"}>
+                <button type="button" className="btn btn-dark btn-outline-black btn-sm px-3">
+                    <i className="fa fa-solid fa-tv"> View</i>
+                </button>
+                <button type="button" className="btn btn-dark btn-outline-primary btn-sm px-3">
+                    <i className="fa fa-solid fa-download"> Download</i>
+                </button>
+            </div>
+        </MDBCard>
+    )
+}
+
+export default MyReports;

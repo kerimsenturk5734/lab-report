1a223873cd7aca5a455015d57c0308263044cbbd
Kerim Şentürk <kerimsenturk2002@outlook.com>
2024-01-25 16:11:56 +0100

    UpdateUserUseCase.ts implemented

---
+++ src/main/web-ui/src/domain/usecase/user/UpdateUserUseCase.ts
@@ -0,0 +1,65 @@
+import {useState} from "react";
+import userDao from "../../../data/api/dao/UserDao";
+import {UpdateUserRequest} from "../../payload/request/UpdateUserRequest";
+
+export const useUpdateUserCase = () => {
+    const [state, setState] = useState({
+        successMessage:'',
+        error: {},
+        errorMessage:'',
+        isLoading: false,
+    });
+
+    const updateUser = async (updateUserRequest : UpdateUserRequest) => {
+        setState({ ...state, isLoading: true });
+
+        await userDao.updateUser(updateUserRequest)
+            .then((res) => {
+                console.log(res)
+                setState({
+                    successMessage: res.data.message,
+                    error: {},
+                    errorMessage: '',
+                    isLoading: false
+                });
+            })
+            .catch((err) => {
+                console.log(err)
+
+                if(err.code == "ERR_NETWORK"){
+                    setState({
+                        successMessage: '',
+                        error: err,
+                        errorMessage: err.message,
+                        isLoading: false
+                    })
+                }
+                else if(err.response.status == 403){
+                    setState({
+                        successMessage: '',
+                        error: err,
+                        errorMessage: "Authentication required!!!",
+                        isLoading: false
+                    });
+                }
+                else if(err.response.status == 400){
+                    setState({
+                        successMessage: '',
+                        error: err,
+                        errorMessage: JSON.stringify(err.response.data, null, 4),
+                        isLoading: false
+                    });
+                }
+                else{
+                    setState({
+                        successMessage: '',
+                        error: err,
+                        errorMessage: err.response.data.message,
+                        isLoading: false
+                    });
+                }
+            })
+    };
+
+    return { state, updateUser };
+};

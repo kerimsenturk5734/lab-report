adf6706c958cd1284d5192ad7a2ba4fd6c7a426e
Kerim Şentürk <kerimsenturk2002@outlook.com>
2024-01-26 18:24:28 +0100

    updated

--- src/main/web-ui/src/data/api/api.js
+++ src/main/web-ui/src/data/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const ROOT_URL = 'http://localhost:8080';
 const API_VERSION_URL = '/v1/api'
-const TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2NTQ1ODE2NTQwNCIsImlzcyI6ImNvbS5rZXJpbXNlbnR1cmsubGFicmVwb3J0IiwiaWF0IjoxNzA2MTk0NTI4LCJleHAiOjE3MDYyMjQ1Mjh9.BKdDOLAf2bEkGHtxOgsbDRPLTG55ljjBTDG6L7Mz-wY" //Get from local storage
+const TOKEN = "TOKEN" //Get from local storage
 
 export const api = axios.create({
     baseURL: ROOT_URL.concat(API_VERSION_URL),
